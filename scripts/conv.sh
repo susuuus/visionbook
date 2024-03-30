e8a6c7a007e5ef4dded62529c7f0c5070665eff9
Shen Shen <shenshen@mit.edu>
2024-03-30 10:11:28 -0400

    better conv script

--- scripts/conv.sh
+++ scripts/conv.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 echo $SCRIPT_DIR
 input_file="$1"
@@ -18,13 +17,14 @@ base_name=$(basename "$input_file" .tex)
 output_file="${base_name}.qmd"
 
 TEMP_TEX="${input_file%.tex}-temp.tex" # Create a temp file name based on the input file
-
-# sed 's/\\marginnote{\(.*\)}/:::{.column-margin}\1:::/g' "$input_file" > "$TEMP_TEX"
-python3 $SCRIPT_DIR/process_marginnote.py "$input_file" "$TEMP_TEX"
+pandoc "$input_file" -s -o "$TEMP_TEX" -f latex -t latex
+python3 $SCRIPT_DIR/process_marginnote.py "$TEMP_TEX" "$TEMP_TEX"
+echo "margin done"
 python3 $SCRIPT_DIR/process_figureref.py "$TEMP_TEX" "$TEMP_TEX"
+echo "fig done"
 # Run Pandoc conversion
 pandoc "$TEMP_TEX" -o "$output_file" -f latex  -t markdown --filter $SCRIPT_DIR/filter.py
-
+echo "filter done"
 
 # remove the [x in] or [x pt]
 # sed -i '' -E 's/\\\[\-?\([0-9]*\)\(\.[0-9]+\)\?\(in\|cm\|pt\)\\\]//g' "$output_file"
@@ -35,12 +35,11 @@ sed -i '' -E 's/#fig:([^ ]+)/#fig-\1/g' "$output_file"
 sed -i '' 's/\\\\linewidth//g' "$output_file"
 sed -i '' -E 's/#fig:([^ ]+)/#fig-\1/g' "$output_file"
 sed -i '' 's/\\@fig-/@fig-/g' "$output_file"
-
-
+sed -i '' 's/sec:\(.*\)/sec-\1/g; s/chap:\(.*\)/chap-\1/g; s/chapter:\(.*\)/chapter-\1/g; s/eq:\(.*\)/eq-\1/g' "$output_file"
+sed -i '' 's/\[image\]/\[\]/g' "$output_file"
+sed -i '' 's/height="\\\\textheight"//g' "$output_file"
 sed -i '' -E 's/(:::\{\.column-margin\})(.*)(:::)/\1\n\2\n\3/g' "$output_file"
 
-
-sed -i '' 's/sec:\(.*\)/sec-\1/g; s/chap:\(.*\)/chap-\1/g; s/chapter:\(.*\)/chapter-\1/g; s/eq:\(.*\)/eq-\1/g' "$output_file"
 
 sed -i '' -e 's/\\img/\\ell/g' \
     -e 's/\\imgin/\\ell\\_{\\texttt{in}}/g' \
@@ -55,7 +54,4 @@ sed -i '' -e 's/\\img/\\ell/g' \
     -e 's/\\lightfield/L/g' "$output_file"
 
 python3 $SCRIPT_DIR/process_marginnoteline.py "$output_file" "$output_file"
-
-sed -i '' 's/\[image\]/\[\]/g' "$output_file"
-
 echo "Conversion completed: $output_file"
